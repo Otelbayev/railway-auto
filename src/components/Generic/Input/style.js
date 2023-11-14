14787683ef413685a2d017260b2fdf4c94249e6f
Jasurbek <jasurdev1604@gmail.com>
2023-11-15 03:21:59 +0500

    Routes

--- src/components/Generic/Input/style.js
+++ src/components/Generic/Input/style.js
@@ -9,12 +9,12 @@ export const Container = styled.input`
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 3px;
+  border-radius: 6px;
   min-width: 120px;
 
   font-size: ${({ fontSize }) => (fontSize ? `${fontSize}px` : "14px")};
   width: ${({ width }) => (width ? `${width}px` : "100%")};
-  height: ${({ height }) => (height ? `${height}px` : "44px")};
+  height: ${({ height }) => (height ? `${height}px` : "33px")};
   border: 1px solid #e6e9ec;
   outline: none;
   padding-left: ${({ icon }) => (icon ? "40px" : "15px")};
