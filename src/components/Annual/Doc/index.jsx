cc41675e89e214e92786fc1fb98f487314d37d26
Jasurbek <jasurdev1604@gmail.com>
2023-11-22 19:34:25 +0500

    PDF

--- src/components/Annual/Doc/index.jsx
+++ src/components/Annual/Doc/index.jsx
@@ -1,11 +1,23 @@
-import React from "react";
-import { Container, Wrapper } from "./style";
+import React, { useContext } from "react";
+import { Container, Wrapper, Table, Top, Title } from "./style";
 import html2pdf from "html2pdf.js";
 import { useParams } from "react-router-dom";
+import { PlanContext } from "../../../context/PlanContext";
 
 const Doc = () => {
   const param = useParams();
-
+  const [data] = useContext(PlanContext);
+  const res = data.filter(
+    (item) =>
+      item.model === "3ВЛ80с" ||
+      item.model === "ВЛ60к" ||
+      item.model === "2ВЛ60с" ||
+      item.model === "O'Z-EL"
+  );
+  const res1 = data.filter(
+    (item) => item.model === "UzTE16M2" || item.model.includes("ТЭ10М")
+  );
+  const res2 = data.filter((item) => item.model.includes("ТЭМ"));
   const convertToPdf = () => {
     const wrapper = document.getElementById("wrapper");
 
@@ -24,25 +36,152 @@ const Doc = () => {
     }
   };
 
+  let nums = 0;
+  let nums1 = 0;
+
   return (
     <Container>
       <Wrapper>
         <div id="wrapper">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-            autem molestias a eius magnam ipsa, nemo provident. Voluptatem quia
-            ducimus perferendis, maxime ex est odit recusandae tempora
-            repudiandae minus laudantium. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Recusandae tenetur qui vero at quis expedita
-            corrupti, non iure praesentium distinctio ullam eaque fugit quas
-            beatae velit molestiae adipisci? Eum obcaecati, voluptates qui
-            tempore earum dolore architecto quisquam reprehenderit debitis
-            inventore! Deserunt nihil distinctio rem ducimus voluptates laborum
-            voluptatibus ipsum cumque ea cupiditate necessitatibus delectus quas
-            inventore tempora nam iure aspernatur earum repellendus ab
-          </p>
+          <Top>
+            <p>Tastiqlayman</p>
+            <p>"O'zbekiston temir yo'llari" AJ</p>
+            <p>bosh menejer- bosh muhandis a.b</p>
+            <p>_________________ Z.E.Maxamatov</p>
+            <p>"___"_________________{param.year} yil</p>
+          </Top>
+          <Title>
+            {param.year} yilning 1-choragi uchun "O'ztrmityolmashtamit" unitar
+            kotxonasida, "O'zbekiston" , "Timchlik" lokomativ depolarida
+            lokomativlarni KRP, KRP-1 mikdorda kapital ta'mirdan hamda TR-3
+            mikdorda joriy ta'mirdan chiqish jadvali
+          </Title>
+          <Table
+            border={1}
+            style={{
+              borderCollapse: "collapse",
+              fontSize: "14px",
+            }}
+          >
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>Локомотив русуми ва рақами</th>
+                <th>Локомотив рўйхатдан ўтган депо</th>
+                <th>Таъмирлаш тури</th>
+                <th>Таъмирлаш жойи</th>
+                <th>Таъмирдан чикиши</th>
+                <th>Секциялар сони</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan={8}>
+                  O'Z-EL, ВЛ60к, 3ВЛ80с ,2ВЛ60с rusumli elektrovozlar boyicha
+                </td>
+              </tr>
+              {res.map(
+                ({
+                  id,
+                  model,
+                  number,
+                  depo,
+                  repairMode,
+                  repairPlace,
+                  outRepair,
+                  section,
+                }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{id}</td>
+                      <td>
+                        {model} {number}
+                      </td>
+                      <td>{depo}</td>
+                      <td>{repairMode}</td>
+                      <td>{repairPlace}</td>
+                      <td>{outRepair}</td>
+                      <td>{section}</td>
+                    </tr>
+                  );
+                }
+              )}
+              <tr>
+                <td colSpan={6}>Jami:</td>
+                <td colSpan={2}>ta seksiya</td>
+              </tr>
+              <tr>
+                <td colSpan={8}>
+                  UzTE16M2 , ТЭ10М ,rusumli Teplovozlar boyicha
+                </td>
+              </tr>
+              {res1.map(
+                ({
+                  id,
+                  model,
+                  number,
+                  depo,
+                  repairMode,
+                  repairPlace,
+                  outRepair,
+                  section,
+                }) => {
+                  nums++;
+                  return (
+                    <tr key={id}>
+                      <td>{nums}</td>
+                      <td>
+                        {model} {number}
+                      </td>
+                      <td>{depo}</td>
+                      <td>{repairMode}</td>
+                      <td>{repairPlace}</td>
+                      <td>{outRepair}</td>
+                      <td>{section}</td>
+                    </tr>
+                  );
+                }
+              )}
+              <tr>
+                <td colSpan={6}>Jami:</td>
+                <td colSpan={2}>ta seksiya</td>
+              </tr>
+              <tr>
+                <td colSpan={8}>ТМЭ-3, ТМЭ-2 rusumli Teplovozlar boyicha</td>
+              </tr>
+              {res2.map(
+                ({
+                  id,
+                  model,
+                  number,
+                  depo,
+                  repairMode,
+                  repairPlace,
+                  outRepair,
+                  section,
+                }) => {
+                  nums1++;
+                  return (
+                    <tr key={id}>
+                      <td>{nums1}</td>
+                      <td>
+                        {model} {number}
+                      </td>
+                      <td>{depo}</td>
+                      <td>{repairMode}</td>
+                      <td>{repairPlace}</td>
+                      <td>{outRepair}</td>
+                      <td>{section}</td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+          </Table>
         </div>
-        <button onClick={convertToPdf}>download</button>
+        <button style={{ margin: "10px 0" }} onClick={convertToPdf}>
+          download
+        </button>
       </Wrapper>
     </Container>
   );
--- src/components/Annual/Doc/style.js
+++ src/components/Annual/Doc/style.js
@@ -11,6 +11,40 @@ export const Wrapper = styled.div`
   background: #fff;
   width: 1100px;
   font-family: "Times New Roman", Times, serif;
-  padding: 25px;
+  padding: 20px 50px;
   box-shadow: 0 0 10px gray;
+  & > div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+`;
+
+export const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  th {
+    padding: 10px;
+  }
+  td {
+    text-align: center;
+    padding: 3px;
+    font-size: 14px;
+  }
+`;
+
+export const Top = styled.div`
+  text-align: center;
+  width: 100%;
+  font-size: 14px;
+  p {
+    padding: 5px 0;
+  }
+`;
+
+export const Title = styled.div`
+  width: 100%;
+  text-align: center;
+  padding: 10px 0;
 `;
