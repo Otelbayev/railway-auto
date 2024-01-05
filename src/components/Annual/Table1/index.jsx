6f46bbc3af8557a3b9751c681314892da04a079a
Jasurbek <jasurdev1604@gmail.com>
2024-01-05 12:57:11 +0500

    Annual plan complate functionally

--- src/components/Annual/Table1/index.jsx
+++ src/components/Annual/Table1/index.jsx
@@ -133,7 +133,7 @@ const Table = () => {
           </div>
         </div>
         <div className="doctitle">
-          Lokomativlardan foydalanish boshqarmasining 2023 yilga mo'ljallangan
+          Lokomativlardan foydalanish boshqarmasining 2024 yilga mo'ljallangan
           joriy, kapital va xizmat muddati uzaytirilgan holda kapital ta'mirlash
           uchun yillik
           <div>prognoz</div>
